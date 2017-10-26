9d6a7de3d76d0d75b48e561e2424bdf2becdcf8e
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-10-26 21:55:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {artist : "", song : ""}
 function updatePlaylist(objPL,artist,song) {
   return Object.assign({}, objPL, {[artist]: song})
 }
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
   return playlist
 }
