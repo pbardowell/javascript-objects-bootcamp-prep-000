33f174f9713a149e0e5644b10737a69ada615449
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-10-27 03:01:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
-var playlist = {artist:"song"}
+var playlist = {madonna:"Like a Virgin"}
 function updatePlaylist(objPL,artist,song) {
   return Object.assign({}, objPL, {[artist]: song})
 }
 function removeFromPlaylist(playlist,artist) {
-  delete playlist.artist
+  delete playlist[artist]
   return playlist
 }
